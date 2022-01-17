ce598d43c337c5ecabcc0a3e61a8d1dcf1814388
Jari <JaAnnala@gmail.com>
2022-01-17 19:29:21 +0200

    implement working search bar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import './App.css';
 import SearchBar from './components/search_bar';
 import axios from 'axios';
@@ -8,57 +8,44 @@ import { searchMovies } from './movie_service';
 const API_KEY = '7d4a9a79104d85fe4518aee4ee95fd56';
 const ROOT_URL ='https://api.themoviedb.org';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-
-    this.state = {
-      movies: []
-    };
-
-  }
-
-  componentDidMount() {
-    //this.discoverMovies();
-    this.searchMovies('spiderman');
-  }
+function App() {
+  
+  const [movies, setMovies] = useState([]);
 
 
-  discoverMovies() {
+  useEffect(() => {
+    discoverMovies();
+  }, []) 
+   
+  const discoverMovies = () => {
 
     axios.get(`${ROOT_URL}/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`)
       .then(response => {
-        this.setState ({
-          movies: response.data.results
-        });
+       setMovies(response.data.results);
       })
       .catch(error => {
       console.log('Error fetching and parsing data', error);
     });
   }
 
-  searchMovies(searchTerm) {
-
+  const fetchMovies = (searchTerm) => {
     searchMovies(searchTerm)
       .then(response => {
-        this.setState ({
-          movies: response.data.results
-        });
+        console.log(response);
+        setMovies(response.data.results);
       })
       .catch(error => {
       console.log('Error fetching and parsing data', error);
     });
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Moviepicker v0.1</h1>
-        <SearchBar onInputChange={this.discoverMovies} />
-        <MovieList movies={this.state.movies}/>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Moviepicker v0.1</h1>
+      <SearchBar handleSearch={fetchMovies} />
+      <MovieList movies={movies}/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,31 +4,50 @@ class SearchBar extends Component {
   constructor(props) {
 		super(props);
 
-		this.state = { term: '' };
+		this.state = { 
+      term: '',
+      lastSearchedTerm: '',
+    };
+    
 	}
 
   handleBlur() {
-    console.log(this.state.term);
+    const term = this.state.term;
+    this.searchMovies(this.state.term);
+  }
+  
+  searchMovies(term) {
+    
+    if(this.state.lastSearchedTerm === term) {
+      console.log('same term, returning');
+      return;
+    }
+    
+    this.props.handleSearch(term);
+    this.setState({
+      lastSearchedTerm: term
+    });
   }
 
+  
+  onInputChange(term) {
+    this.setState({term});
+  }
+  
+  handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      this.searchMovies(this.state.term);
+    }
+  }
+  
   render() {
     return(
       <div>
         <div className="search-bar">
-          <input onChange={e => this.onInputChange(e.target.value)} onBlur={() => this.handleBlur()} onKeyUp={() => this.handleKeyPress()} />
+          <input onChange={e => this.onInputChange(e.target.value)} onBlur={() => this.handleBlur()} onKeyUp={(e) => this.handleKeyPress(e)} />
         </div>
       </div>
     );
-  }
-
-  onInputChange(term) {
-    this.setState({term});
-  }
-
-  handleKeyPress(e) {
-    if (e.key === 'Enter') {
-      
-    }
   }
 }
 
